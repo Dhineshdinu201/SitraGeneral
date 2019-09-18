f30437c938c77d37b6d060d573d7017e5baa5001
dhinesh <dhineshdinu201@gmail.com>
2019-09-18 22:10:46 +0530

    check1

--- src/main/java/com/learning/sitrageneral/MainActivity.java
+++ src/main/java/com/learning/sitrageneral/MainActivity.java
@@ -492,7 +492,6 @@ public void onResume(){
 
     }
 
-    //alertDialogg
     public void showSpecialService(){
         Activity activity = null;
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
